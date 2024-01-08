9b8af430b4922516180db4eb4d8be93711667207
sadeg-akbarian <sadeg.akbarian@gmail.com>
2024-01-08 01:10:16 +0100

    feat: task 'todo-app-with-vue' added

---
+++ todo-app-with-vue/src/lib.js
@@ -0,0 +1,70 @@
+export async function doTheUpdate(changedToDo, urlOfChanged) {
+  changedToDo.done = !changedToDo.done;
+  const response = await fetch(urlOfChanged + changedToDo.id, {
+    method: "PUT",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(changedToDo),
+  });
+  if (!response.ok) {
+    alert("ERROR: This didn't work!!!");
+  }
+}
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+export function removeDoneToDos(localToDoList, urlOfToDoListServer) {
+  const toDoListWithoutDoneToDos = [];
+  for (let toDo of localToDoList) {
+    if (toDo.done) {
+      fetch(urlOfToDoListServer + toDo.id, {
+        method: "DELETE",
+      });
+    } else {
+      toDoListWithoutDoneToDos.push(toDo);
+    }
+  }
+  return toDoListWithoutDoneToDos;
+}
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+export function addedToDoList(
+  localToDoList,
+  maybeNewToDo,
+  urlOfToDoListServer
+) {
+  let isSuggestedNewToDoAlreadyInToDoList = false;
+  for (let toDo of localToDoList) {
+    if (maybeNewToDo.toLowerCase() === toDo.description.toLowerCase()) {
+      isSuggestedNewToDoAlreadyInToDoList = true;
+    }
+  }
+  if (!isSuggestedNewToDoAlreadyInToDoList) {
+    const newToDo = {
+      description: maybeNewToDo,
+      done: false,
+    };
+    addNewToDoToList(urlOfToDoListServer, newToDo, localToDoList);
+    return "";
+  } else {
+    return "inline";
+  }
+}
+
+// ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+export async function addNewToDoToList(
+  urlOfToListServer,
+  newListItem,
+  localToDoList
+) {
+  const response = await fetch(urlOfToListServer, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(newListItem),
+  });
+  const result = await response.json();
+  localToDoList.push(result);
+}
